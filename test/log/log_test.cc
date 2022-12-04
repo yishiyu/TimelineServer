5d5396ebcaf512e498a801686cb046fa052ead21
yishiyu <814384981@qq.com>
2022-12-04 22:19:05 +0800

    完成log及其test

--- test/log/log_test.cc
+++ test/log/log_test.cc
@@ -16,12 +16,27 @@ using std::string;
 namespace TimelineServer {
 
 TEST(Log, sync_log) {
-  Log::get_instance()->init(LOG_LEVEL::ELL_DEBUG, "../data/log", ".log", 0);
+  Log::get_instance()->init(LOG_LEVEL::ELL_INFO, "../data/log", ".log", 0);
 
+  LOG_INFO("%s", "============== sync_log =============")
+  LOG_DEBUG("%s", "a debug message written by sync_log");
+  LOG_INFO("%s", "an info message written by sync_log");
+  LOG_WARN("%s", "a warning message written by sync_log");
+  LOG_ERROR("%s", "an error message written by sync_log");
+  LOG_INFO("%s", "(不应该记录 DEBUG 日志)");
+  LOG_INFO("%s", "=====================================")
+}
 
-  LOG_DEBUG("%s", "debug log");
+TEST(Log, async_log) {
+  Log::get_instance()->init(LOG_LEVEL::ELL_DEBUG, "../data/log", ".log", 100);
 
-  Log::get_instance()->flush();
+  LOG_INFO("%s", "============= async_log =============")
+  LOG_DEBUG("%s", "a debug message written by async_log");
+  LOG_INFO("%s", "an info message written by async_log");
+  LOG_WARN("%s", "a warning message written by async_log");
+  LOG_ERROR("%s", "an error message written by async_log");
+  LOG_INFO("%s", "(应该记录 DEBUG 日志)");
+  LOG_INFO("%s", "=====================================")
 }
 
 }  // namespace TimelineServer
