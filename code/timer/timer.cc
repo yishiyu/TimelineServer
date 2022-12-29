e1b90cd09b29967db5cd3de8e74a9da72c48babc
yishiyu <814384981@qq.com>
2022-12-29 16:43:15 +0800

    初步完成timer

---
+++ code/timer/timer.cc
@@ -0,0 +1,154 @@
+#include "timer.h"
+
+namespace TimelineServer {
+
+void Timer::add_timer(timer_id id, int timeout, timeout_cb& call_back) {
+  assert(id >= 0);
+  size_t i;
+
+  if (timer_ref_.count(id) == 0) {
+    // 新节点
+    i = heap_.size();
+    timer_ref_[id] = i;
+    heap_.push_back({id, clock::now() + ms(timeout), call_back});
+    siftup_(i);
+  } else {
+    // 已有节点
+    i = timer_ref_[id];
+    heap_[i].expires = clock::now() + ms(timeout);
+    heap_[i].cb = call_back;
+
+    // 调整该元素位置
+    if (!siftdown_(i, heap_.size())) {
+      siftup_(i);
+    }
+  }
+}
+
+void Timer::pop_timer() {
+  assert(!heap_.empty());
+  delete_(0);
+}
+
+void Timer::do_work(timer_id id) {
+  if (heap_.empty() || timer_ref_.count(id) == 0) {
+    return;
+  }
+
+  size_t i = timer_ref_[id];
+  TimerNode& node = heap_[i];
+  node.cb();
+  delete_(i);
+}
+
+void Timer::adjust(timer_id id, int timeout) {
+  // 保证该元素存在
+  assert(!heap_.empty() && timer_ref_.count(id) > 0);
+  // 调整触发时间
+  heap_[timer_ref_[id]].expires = clock::now() + ms(timeout);
+
+  // 调整该元素位置
+  if (!siftdown_(timer_ref_[id], heap_.size())) {
+    siftup_(timer_ref_[id]);
+  }
+}
+
+void Timer::clear() {
+  timer_ref_.clear();
+  heap_.clear();
+}
+
+void Timer::tick() {
+  if (heap_.empty()) {
+    return;
+  }
+
+  while (!heap_.empty()) {
+    TimerNode& node = heap_.front();
+    ms time_left = std::chrono::duration_cast<ms>(node.expires - clock::now());
+    if (time_left.count() > 0) {
+      break;
+    }
+
+    // 触发该定时器
+    node.cb();
+    pop_timer();
+  }
+}
+
+int Timer::get_next_timeout() {
+  tick();
+
+  size_t res = -1;
+  if (!heap_.empty()) {
+    auto expires = heap_.front().expires;
+    res = std::chrono::duration_cast<ms>(expires - clock::now()).count();
+  }
+
+  return res;
+}
+
+// 私有函数
+
+void Timer::delete_(size_t i) {
+  assert(!heap_.empty() && i >= 0 && i < heap_.size());
+  size_t n = heap_.size() - 1;
+
+  // 将该位置元素直接换到最后
+  // 再调整原堆尾元素位置
+  if (i < n) {
+    swap_(i, n);
+    if (!siftdown_(i, n)) {
+      siftup_(i);
+    }
+  }
+
+  // 删除堆尾元素
+  timer_ref_.erase(heap_.back().id);
+  heap_.pop_back();
+}
+
+void Timer::siftup_(size_t i) {
+  assert(i >= 0 && i < heap_.size());
+  // 二叉树中的父节点
+  size_t j = (i - 1) / 2;
+
+  while (j >= 0) {
+    // 父节点小于当前节点,停止传递
+    if (heap_[j] < heap_[i]) break;
+    swap_(i, j);
+    i = j;
+    j = (i - 1) / 2;
+  }
+}
+
+bool Timer::siftdown_(size_t i, size_t n) {
+  assert(i >= 0 && i < heap_.size());
+  assert(n >= 0 && n <= heap_.size());
+  int init_pos = i;
+  // 二叉树中的左子结点
+  size_t j = i * 2 + 1;
+
+  while (j < n) {
+    // 查找子结点中较小的(否则交换后新节点不满足小于子结点的要求)
+    if (j + 1 < n && heap_[j + 1] < heap_[j]) j++;
+    // 父节点小于当前节点,停止传递
+    if (heap_[i] < heap_[j]) break;
+    swap_(i, j);
+    i = j;
+    j = i * 2 + 1;
+  }
+
+  // 返回是否进行过交换
+  return i > init_pos;
+}
+
+void Timer::swap_(size_t i, size_t j) {
+  assert(i >= 0 && i < heap_.size());
+  assert(j >= 0 && j < heap_.size());
+  std::swap(heap_[i], heap_[j]);
+  timer_ref_[heap_[i].id] = i;
+  timer_ref_[heap_[j].id] = j;
+}
+
+}  // namespace TimelineServer
---
+++ code/timer/timer.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <assert.h>
+
+#include <chrono>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+typedef std::function<void()> timeout_cb;
+typedef unsigned int timer_id;
+typedef std::chrono::high_resolution_clock clock;
+typedef std::chrono::microseconds ms;
+typedef clock::time_point time_stamp;
+
+namespace TimelineServer {
+
+struct TimerNode {
+  timer_id id;
+  time_stamp expires;
+  timeout_cb cb;
+  bool operator<(const TimerNode& t) { return expires < t.expires; }
+};
+
+class Timer {
+ public:
+  Timer() { heap_.reserve(64); };
+  ~Timer() { clear(); };
+
+  // 增加/删除定时器
+  void add_timer(timer_id id, int timeout, timeout_cb& call_back);
+  void pop_timer();
+
+  // 调用某个定时器的回调函数/重新调整某个定时器的时间
+  void do_work(timer_id id);
+  void adjust(timer_id id, int timeout);
+
+  // 删除所有定时器/查询定时器是否到时间,激活并删除
+  void clear();
+  void tick();
+
+  // 获取距离下一个定时器触发的时间差
+  int get_next_timeout();
+
+ private:
+  void delete_(size_t i);
+  void siftup_(size_t i);
+  bool siftdown_(size_t i, size_t n);
+
+  void swap_(size_t i, size_t j);
+  
+  // 小顶堆,0下标对应最小元素
+  std::vector<TimerNode> heap_;
+  std::unordered_map<timer_id, size_t> timer_ref_;
+};
+
+}  // namespace TimelineServer
