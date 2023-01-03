58d541048fe73f14e7ff7a58be46cb184c5382ed
yishiyu <814384981@qq.com>
2023-01-03 16:26:06 +0800

    完善timer的测试

--- test/timer/timer_test.cc
+++ test/timer/timer_test.cc
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;
 using std::cout;
@@ -15,19 +16,100 @@ using std::string;
 
 namespace TimelineServer {
 
-void hello() { cout << "hello world" << endl; }
+// 继承自被测试类,其中的数据可以被多个测试函数使用
+class TimerTest : public ::testing::Test {
+ protected:
+  // // 初始化数据
+  // void SetUp() override {}
 
-TEST(Timer, add_timer) {
+  // override TearDown 来清理数据
+  void TearDown() override { timer.clear(); }
+
   Timer timer;
-  timer_id id = 111;
-  timer.add_timer(id, 3000, &hello);
+};
 
-  sleep(2);
-  cout << "after 2 seconds" << endl;
-  timer.tick();
-  sleep(2);
-  cout << "after 2 seconds" << endl;
-  timer.tick();
+void timeout_message(string msg) { cout << msg << endl; }
+
+TEST_F(TimerTest, add_timer) {
+  std::vector<int> timer_delay = {3000, 4000, 5000};
+
+  char message[1024] = {0};
+
+  for (int delay : timer_delay) {
+    sprintf(message, "timer %d is called", delay);
+    timer.add_timer(delay, delay, std::bind(timeout_message, string(message)));
+  }
+  while (timer.get_next_timeout() > 0) {
+    sleep(1);
+  }
+
+  timer.clear();
+}
+
+TEST_F(TimerTest, pop_timer) {
+  std::vector<int> timer_delay = {11238, 1537, 983, 123, 1380,
+                                  4382,  9382, 23,  4659};
+
+  char message[1024] = {0};
+
+  for (int delay : timer_delay) {
+    sprintf(message, "timer %d is called", delay);
+    timer.add_timer(delay, delay, std::bind(timeout_message, string(message)));
+  }
+
+  // 删除最前面两个定时器
+  timer.pop_timer();
+  timer.pop_timer();
+
+  while (timer.get_next_timeout() > 0) {
+    sleep(1);
+  }
+
+  timer.clear();
+}
+
+TEST_F(TimerTest, do_work) {
+  std::vector<int> timer_delay = {3000, 4000, 5000};
+
+  char message[1024] = {0};
+
+  for (int delay : timer_delay) {
+    sprintf(message, "timer %d is called", delay);
+    timer.add_timer(delay, delay, std::bind(timeout_message, string(message)));
+  }
+
+  // 直接执行4000id的任务
+  cout << "========do work========" << endl;
+  timer.do_work(4000);
+  cout << "=======================" << endl;
+
+  while (timer.get_next_timeout() > 0) {
+    sleep(1);
+  }
+
+  timer.clear();
+}
+
+TEST_F(TimerTest, adjust) {
+  std::vector<int> timer_delay = {3000, 4000, 5000};
+
+  char message[1024] = {0};
+
+  for (int delay : timer_delay) {
+    sprintf(message, "timer %d is called", delay);
+    timer.add_timer(delay, delay, std::bind(timeout_message, string(message)));
+  }
+
+  // 修改4000id的任务的时间
+  cout << "========do work========" << endl;
+  cout << "timer 4000 is adjusted" << endl;
+  sprintf(message, "timer %d is called", 4000);
+  timer.add_timer(4000, 6000, std::bind(timeout_message, string(message)));
+  cout << "=======================" << endl;
+
+  while (timer.get_next_timeout() > 0) {
+    sleep(1);
+  }
 
   timer.clear();
 }
