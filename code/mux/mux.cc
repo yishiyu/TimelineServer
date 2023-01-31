669e794c6d71d2f0926c9c23f330f5bf8e6e8998
yishiyu <814384981@qq.com>
2023-01-31 21:32:21 +0800

    添加mux

---
+++ code/mux/mux.cc
@@ -0,0 +1,61 @@
+#include "mux.h"
+
+namespace TimelineServer {
+
+Mux::Mux(int max_event) : mux_fd_(epoll_create(max_event)), events_(max_event) {
+  assert(mux_fd_ >= 0 && events_.size() > 0);
+}
+
+Mux::~Mux() { close(mux_fd_); }
+
+bool Mux::add_fd(int fd, uint32_t events) {
+  if (fd < 0) {
+    return false;
+  }
+
+  epoll_event ev = {0};
+  ev.data.fd = fd;
+  ev.events = events;
+
+  return 0 == epoll_ctl(mux_fd_, EPOLL_CTL_ADD, fd, &ev);
+}
+
+bool Mux::mod_fd(int fd, uint32_t events) {
+  if (fd < 0) {
+    return false;
+  }
+
+  epoll_event ev = {0};
+  ev.data.fd = fd;
+  ev.events = events;
+
+  return 0 == epoll_ctl(mux_fd_, EPOLL_CTL_MOD, fd, &ev);
+}
+
+bool Mux::del_fd(int fd) {
+  if (fd < 0) {
+    return false;
+  }
+
+  epoll_event ev = {0};
+
+  return 0 == epoll_ctl(mux_fd_, EPOLL_CTL_DEL, fd, &ev);
+}
+
+int Mux::wait(int timeout) {
+  events_count_ = epoll_wait(mux_fd_, &events_[0],
+                             static_cast<int>(events_.size()), timeout);
+  return events_count_;
+}
+
+int Mux::get_signal_fd(size_t i) const {
+  assert(i < events_count_ && i < events_.size() && i >= 0);
+  return events_[i].data.fd;
+}
+
+int Mux::get_signal_events(size_t i) const {
+  assert(i < events_count_ && i < events_.size() && i >= 0);
+  return events_[i].events;
+}
+
+}  // namespace TimelineServer
---
+++ code/mux/mux.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <assert.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+#include <vector>
+
+namespace TimelineServer {
+
+class Mux {
+ public:
+  explicit Mux(int max_event = 512);
+  ~Mux();
+
+  // 数据源操作函数
+  bool add_fd(int fd, uint32_t events);
+  bool mod_fd(int fd, uint32_t events);
+  bool del_fd(int fd);
+
+  // 查询函数/获取信号来源
+  int wait(int timeout = -1);
+  int get_signal_fd(size_t i) const;
+  int get_signal_events(size_t i) const;
+
+ private:
+  int mux_fd_;
+
+  // 当前触发事件的信号源数量
+  size_t events_count_;
+  std::vector<struct epoll_event> events_;
+};
+
+}  // namespace TimelineServer
