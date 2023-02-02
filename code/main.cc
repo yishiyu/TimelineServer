556a5fe77c459748ffb85ec1c3bfe03262395f56
yishiyu <814384981@qq.com>
2023-02-02 17:27:00 +0800

    修改根目录路径

--- code/main.cc
+++ code/main.cc
@@ -8,7 +8,7 @@ int main() {
   string sql_user = "root";
   string sql_pwd = "explosion";
   string sql_db_name = "timelineserver";
-  string src_dir = "/home/yishiyu/Code/TimelineServer/";
+  string src_dir = "./";
 
   TimelineServer::Server server(2333, true, 60000, true, src_dir, sql_host,
                                 3306, sql_user, sql_pwd, sql_db_name, 1, 1,
